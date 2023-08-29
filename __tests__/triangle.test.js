525870fc8002ecc9b8d1fc58b134e38ec86f930f
Joey Palchak <joey.palchak@gmail.com>
2023-08-28 22:02:46 -0700

    Configure Jest with --coverage flag.

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -24,6 +24,11 @@ describe('Triangle', () => {
     expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
   });
 
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5);
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
+
 
 });
 
